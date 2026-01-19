17437022e08b77168238560f99ee0578342d1385
Ahmed Eid <>
2026-01-19 20:48:09 +0200

    Fix

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
 <?php
 
-    $text = $_GET['text'];
+    $text = $_POST['text'];
 
-    $safeText = escapeshellarg($text);
+    $safeText =  escapeshellarg($text . ' ')  ;
 
-    shell_exec("xdotool type $safeText");
+
+
+// Detect language
+    if (preg_match('/[\x{0600}-\x{06FF}]/u', $safeText)) {
+        $layout = 'ar';
+    } else {
+        $layout = 'us';
+    }
+
+// Optional: store current layout
+    $currentLayout = trim(shell_exec("setxkbmap -query | grep layout | awk '{print $2}'"));
+
+// Switch layout
+    shell_exec("setxkbmap $layout");
+
+// Type text
+    shell_exec("xdotool type --delay 5 $safeText");
+
+// Restore layout
+    if ($currentLayout) {
+        shell_exec("setxkbmap $currentLayout");
+    }
+
     header('Location: index.html');
+
+
+
+
 ?>
